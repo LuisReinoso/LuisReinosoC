98dfeb400677affa810bb4c6f5d6605a35abb971
Luis Reinoso <LuisReinoso@users.noreply.github.com>
2024-09-18 12:53:15 -0500

    feat(core): add jest as unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+// import { describe, expect, test , it, beforeEach} from '@jest/globals';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
