993dd248cf6f12daabc4ee459397aa1317fcf079
Luis Reinoso <LuisReinoso@users.noreply.github.com>
2024-09-19 16:22:05 -0500

    fix(register-product): add missing provider to storybook for register product form

--- src/app/pages/register-product-page/components/register-product-form/register-product-form.component.stories.ts
+++ src/app/pages/register-product-page/components/register-product-form/register-product-form.component.stories.ts
@@ -1,8 +1,19 @@
-import type { Meta, StoryObj } from '@storybook/angular';
+import { moduleMetadata, type Meta, type StoryObj } from '@storybook/angular';
 import { RegisterProductFormComponent } from './register-product-form.component';
+import { ProductService } from '@app/core/services/product.service';
 
 const meta: Meta<RegisterProductFormComponent> = {
   component: RegisterProductFormComponent,
+  decorators: [
+    moduleMetadata({
+      providers: [
+        {
+          provide: ProductService,
+          useValue: {},
+        },
+      ],
+    }),
+  ],
 };
 
 export default meta;
