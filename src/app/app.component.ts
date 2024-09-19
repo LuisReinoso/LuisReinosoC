94c0a5326b624ce295f3b7fe07b462c0257c5b10
Luis Reinoso <LuisReinoso@users.noreply.github.com>
2024-09-18 21:37:50 -0500

    feat(core): implement header in whole app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { AlertComponent } from './shared/alert/alert.component';
 import { AlertService } from './shared/alert/alert.service';
 import { AsyncPipe, NgIf } from '@angular/common';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AlertComponent, NgIf, AsyncPipe],
+  imports: [RouterOutlet, AlertComponent, NgIf, AsyncPipe, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
